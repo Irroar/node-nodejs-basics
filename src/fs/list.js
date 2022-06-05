936e83ae64537eb4870481c56c1320c4c799c125
Iryna Lytvynenko <frau4145@gmail.com>
2022-06-05 16:45:01 +0300

    feat: add solution for fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,30 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { readdir, access } from 'fs/promises';
+import CustomFSError from './CustomError.js';
+import { isExists } from './utils/exist.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const list = async () => {
-    // Write your code here 
+  const pathToFolder = join(__dirname, 'files');
+
+  try {
+    const isDirExists = await isExists(pathToFolder);
+
+    if (!isDirExists) {
+      throw new CustomFSError('FS operation failed');
+    }
+
+    const fileNames = await readdir(pathToFolder);
+
+    fileNames.forEach((fileName) => {
+      console.log(fileName);
+    });
+  } catch(err) {
+    console.log(err);
+  }
 };
+
+list();
