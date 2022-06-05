b0c0ef6e610d29f2e2378d0d6ffc5074ad97e07a
Iryna Lytvynenko <frau4145@gmail.com>
2022-06-05 19:15:27 +0300

    feat: add solution for streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,20 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+  const fileName = 'fileToRead.txt';
+  const pathToFile = join(__dirname, 'files', fileName);
+
+  try {
+    const rs = createReadStream(pathToFile);
+    rs.pipe(process.stdout);
+  } catch(err) {
+    console.log(err);
+  }
 };
+
+read();
