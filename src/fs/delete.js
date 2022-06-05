36e667860684aed6ce8a1ce24695d50ae49c3cf7
Iryna Lytvynenko <frau4145@gmail.com>
2022-06-05 16:19:35 +0300

    feat: add solution for fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,21 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { rm } from 'fs/promises';
+import CustomFSError from './CustomError.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const remove = async () => {
-    // Write your code here 
+  const pathToFile = join(__dirname, 'files', 'fileToRemove.txt');
+  
+  try {
+    await rm(pathToFile);
+  } catch(err) {
+    if (err.code === 'ENOENT') { 
+      throw new CustomFSError('FS operation failed'); 
+    }
+  }
 };
+
+remove();
