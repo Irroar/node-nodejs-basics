fa9868f89d72acaf5cfedba68250a6358a4de491
Iryna Lytvynenko <frau4145@gmail.com>
2022-06-05 19:31:35 +0300

    feat: add solution for streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,31 @@
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createInterface } from 'readline';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 export const write = async () => {
-    // Write your code here 
+  const fileName = 'fileToWrite.txt';
+  const pathToFile = join(__dirname, 'files', fileName);
+
+  const rl = createInterface({ input: process.stdin, output: process.stdout });
+
+  try {
+    const ws = createWriteStream(pathToFile);
+    rl.on('line', (line) => { 
+      ws.write(`${line}\n`);
+    });
+    rl.on('close', () => {
+      ws.end();
+      process.exit();
+    });
+  } catch(err) {
+    console.log(err);
+  }
 };
+
+write();
