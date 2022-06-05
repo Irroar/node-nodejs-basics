59886926d056367ea6718036f28d537187b8923d
Iryna Lytvynenko <frau4145@gmail.com>
2022-06-05 16:49:20 +0300

    feat: add solution for fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,27 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+import CustomFSError from './CustomError.js';
+import { isExists } from './utils/exist.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+  const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const isFileExists = await isExists(pathToFile);
+    if (!isFileExists) {
+      throw new CustomFSError('FS operation failed');
+    }
+
+    const rs = createReadStream(pathToFile);
+    rs.pipe(process.stdout);
+
+  } catch(err) {
+    console.log(err);
+  }
 };
+
+read();
