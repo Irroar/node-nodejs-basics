e5f0adf319783b71452e31fdc84de2647a4d8115
Iryna Lytvynenko <frau4145@gmail.com>
2022-06-05 18:06:07 +0300

    fix: fix solution for cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,7 +1,8 @@
 export const parseArgs = () => {
   const args = process.argv.slice(2);
-  for (let i = 0; i < args.length - 1; i += 1) {
-    console.log(`${args[i].slice(2)} is ${args[i + 1]}`);
+  console.log(args)
+  for (let i = 2; i < args.length + 2; i += 2) {
+    console.log(`${args[i - 2].slice(2)} is ${args[i - 1]}`);
   }
 };
 
