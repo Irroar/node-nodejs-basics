31e49536a3367a41c6badbdf621d7665aa867de6
Iryna Lytvynenko <frau4145@gmail.com>
2022-06-05 15:00:48 +0300

    feat: add solution for fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,28 @@
+import { access, appendFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import CustomFSError from './CustomError.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+  const fileName = 'fresh.txt';
+  const pathToFile = join(__dirname, fileName);
+  const content = 'I am fresh and young';
+
+  try {
+    await access(pathToFile);
+    throw new CustomFSError('FS operation failed');
+  }
+  catch(err) {
+    if (err instanceof CustomFSError) { console.log(err); }
+    else { 
+      await appendFile(pathToFile, content);
+      console.log('File was successfuly created') 
+    }
+  }
+
 };
+
+create();
